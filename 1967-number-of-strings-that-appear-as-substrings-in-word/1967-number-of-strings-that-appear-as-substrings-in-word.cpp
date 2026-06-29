f43cf4c37196ecd037877391b8dbdad04774779c
Chanakya Navghare <chanakyanavghare009@gmail.com>
2026-06-29 18:48:30 +0530

    Time: 0 ms (100%), Space: 11.7 MB (61.65%) - LeetHub

--- 1967-number-of-strings-that-appear-as-substrings-in-word/1967-number-of-strings-that-appear-as-substrings-in-word.cpp
+++ 1967-number-of-strings-that-appear-as-substrings-in-word/1967-number-of-strings-that-appear-as-substrings-in-word.cpp
@@ -1,23 +1,37 @@
+// class Solution {
+// public:
+//     int numOfStrings(vector<string>& patterns, string word) {
+
+//         unordered_set<string> sub;
+//         int n =word.size();
+
+//         for (int i = 0; i < n; i++) {
+//             for (int j = 1; j <= n - i; j++) {
+
+//                 sub.insert(word.substr(i, j));
+//             }
+//         }
+
+//         int ans=0;
+//         for(auto x:patterns){
+//             if(sub.count(x)){
+//                 ans++;
+//             }
+//         }
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
     int numOfStrings(vector<string>& patterns, string word) {
+        int ans = 0;
 
-        unordered_set<string> sub;
-        int n =word.size();
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 1; j <= n - i; j++) {
-
-                sub.insert(word.substr(i, j));
-            }
-        }
-
-        int ans=0;
-        for(auto x:patterns){
-            if(sub.count(x)){
+        for (auto &p : patterns) {
+            if (word.find(p) != string::npos)
                 ans++;
-            }
         }
+
         return ans;
     }
 };
