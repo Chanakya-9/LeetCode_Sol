68ec086f71d62b6319b67cc7b9be5bddbc713260
Chanakya Navghare <chanakyanavghare009@gmail.com>
2026-07-07 18:03:58 +0530

    Time: 34 ms (78.24%), Space: 165.9 MB (27.98%) - LeetHub

--- 3756-concatenate-non-zero-digits-and-multiply-by-sum-ii/3756-concatenate-non-zero-digits-and-multiply-by-sum-ii.cpp
+++ 3756-concatenate-non-zero-digits-and-multiply-by-sum-ii/3756-concatenate-non-zero-digits-and-multiply-by-sum-ii.cpp
@@ -1,57 +1,60 @@
 class Solution {
-    int MOD = 1e9 + 7;
+    static const int MOD = 1e9 + 7;
 
 public:
     vector<int> sumAndMultiply(string s, vector<vector<int>>& queries) {
-
         int n = s.size();
 
-        vector<int> digit;
-        vector<int> pos;
+        vector<int> digits;
+        vector<int> idx(n + 1);
 
+        // idx[i] = number of non-zero digits before index i
+        int cnt = 0;
         for (int i = 0; i < n; i++) {
+            idx[i] = cnt;
             if (s[i] != '0') {
-                digit.push_back(s[i] - '0');
-                pos.push_back(i);
+                digits.push_back(s[i] - '0');
+                cnt++;
             }
         }
+        idx[n] = cnt;
 
-        int k = digit.size();
+        int k = digits.size();
 
+        // powers of 10
         vector<long long> pow10(k + 1, 1);
         for (int i = 1; i <= k; i++)
             pow10[i] = (pow10[i - 1] * 10) % MOD;
 
-        vector<long long> pref(k + 1, 0);
+        // prefHash[i] = value of first i non-zero digits
+        vector<long long> prefHash(k + 1, 0);
+
+        // prefSum[i] = sum of first i non-zero digits
         vector<long long> prefSum(k + 1, 0);
 
         for (int i = 0; i < k; i++) {
-            pref[i + 1] = (pref[i] * 10 + digit[i]) % MOD;
-            prefSum[i + 1] = prefSum[i] + digit[i];
+            prefHash[i + 1] = (prefHash[i] * 10 + digits[i]) % MOD;
+            prefSum[i + 1] = prefSum[i] + digits[i];
         }
 
         vector<int> ans;
 
         for (auto& q : queries) {
-
             int l = q[0];
             int r = q[1];
 
-            auto it1 = lower_bound(pos.begin(), pos.end(), l);
-            auto it2 = upper_bound(pos.begin(), pos.end(), r);
+            int L = idx[l];
+            int R = idx[r + 1] - 1;
 
-            if (it1 == it2) {
+            if (L > R) {
                 ans.push_back(0);
                 continue;
             }
 
-            int L = it1 - pos.begin();
-            int R = (it2 - pos.begin()) - 1;
-
             int len = R - L + 1;
 
             long long x =
-                (pref[R + 1] - pref[L] * pow10[len] % MOD + MOD) % MOD;
+                (prefHash[R + 1] - prefHash[L] * pow10[len] % MOD + MOD) % MOD;
 
             long long sum = prefSum[R + 1] - prefSum[L];
 
