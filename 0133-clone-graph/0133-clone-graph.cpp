8088bd83600c2c5aaa3ec330c5e6037b28709d42
Chanakya Navghare <chanakyanavghare009@gmail.com>
2026-07-21 17:32:07 +0530

    Time: 0 ms (100%), Space: 12.3 MB (11.71%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -0,0 +1,44 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+
+    unordered_set<int> check;
+    unordered_map<int,Node*> mp;
+    Node* cloneGraph(Node* node) {
+        if(node==NULL){
+            return node;
+        }
+        if(check.count(node->val)){
+            return mp[node->val];
+        }
+        Node* head=new Node(node->val);
+        check.insert(head->val);
+        mp[head->val]=head;
+
+        for(auto nei:node->neighbors){
+            head->neighbors.push_back(cloneGraph(nei));
+
+        }
+        return head;
+    }
+};
