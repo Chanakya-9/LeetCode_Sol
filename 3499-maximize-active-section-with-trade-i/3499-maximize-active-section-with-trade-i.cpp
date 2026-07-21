58fa963607d840125c4b39b9323f940f8344b60c
Chanakya Navghare <chanakyanavghare009@gmail.com>
2026-07-21 14:35:41 +0530

    Time: 67 ms (56.31%), Space: 80.9 MB (36.04%) - LeetHub

---
+++ 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int maxActiveSectionsAfterTrade(string s) {
+        vector<int> zeros;
+        int n = s.size();
+        int curr = 0;
+        bool con = false;
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (s[i] == '0') {
+                curr++;
+            } else {
+                ans++;
+                if (curr > 0) {
+                    zeros.push_back(curr);
+                }
+                curr = 0;
+            }
+        }
+
+        if (curr > 0) {
+            zeros.push_back(curr);
+        }
+
+        int maxi=0;
+        for(int i=1;i<zeros.size();i++){
+            maxi=max(maxi,zeros[i]+zeros[i-1]);
+        }
+        return ans+maxi;
+
+
+    }
+};
