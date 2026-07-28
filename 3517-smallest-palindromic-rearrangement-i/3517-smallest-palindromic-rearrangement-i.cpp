94684a5b55b6e38cf09ce8ae1d3a66a74b3d57f0
Chanakya Navghare <chanakyanavghare009@gmail.com>
2026-07-28 12:43:04 +0530

    Time: 56 ms (46.59%), Space: 54.1 MB (91.65%) - LeetHub

---
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    string smallestPalindrome(string s) {
+        int n = s.size();
+        vector<int> freq(26, 0);
+        for (int i = 0; i < n; i++) {
+            freq[s[i] - 'a'] ++;
+           
+        }
+
+       
+
+        int solo = -1;
+
+        int p = 0;
+
+        for (int i = 0; i < n / 2; i++) {
+            while(p<26){
+                if(freq[p]>1){
+                    s[i]='a'+p;
+                    s[n-1-i]='a'+p;
+                    freq[p]--;
+                    freq[p]--;
+                    break;
+                }else if(freq[p]==1){
+                    solo = p;
+                }
+                p++;
+            }
+        }
+        if(solo!=-1){
+            s[n/2]='a'+solo;
+        }
+
+        return s;
+
+    }
+};
