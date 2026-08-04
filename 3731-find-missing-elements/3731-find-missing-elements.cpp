16c3fe024d0ccca8197216dbca1289cf12cd3e1f
Chanakya Navghare <chanakyanavghare009@gmail.com>
2026-08-04 13:01:24 +0530

    Time: 7 ms (18.86%), Space: 33.5 MB (38.24%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.cpp
+++ 3731-find-missing-elements/3731-find-missing-elements.cpp
@@ -6,12 +6,12 @@ public:
         int mini=nums[0];
         int maxi=nums[n-1];
         unordered_set<int> st (nums.begin(),nums.end());
-        vector<int> ans;
+        nums={};
         for( int i= mini+1;i<maxi;i++){
             if(st.count(i)==0){
-                ans.push_back(i);
+                nums.push_back(i);
             }
         }
-        return ans;
+        return nums;
     }
 };
