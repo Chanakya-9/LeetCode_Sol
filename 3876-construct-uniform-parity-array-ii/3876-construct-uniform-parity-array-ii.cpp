589688ad786de569fc524966278f821bacef1b55
Chanakya Navghare <chanakyanavghare009@gmail.com>
2026-09-03 20:17:31 +0530

    Time: 1 ms (79.58%), Space: 166 MB (31.94%) - LeetHub

---
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums) {
+        int mn = *min_element(nums.begin(), nums.end());
+        if (mn % 2 != 0) return true;
+
+        for (int x : nums) {
+            if (x % 2 != 0) return false;
+        }
+
+        return true;
+    }
+};
