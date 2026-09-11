feefda147d88fa0b3db8934c2aff33692281a2ec
Chanakya Navghare <chanakyanavghare009@gmail.com>
2026-09-11 20:44:15 +0530

    Time: 0 ms (100%), Space: 28.7 MB (88.78%) - LeetHub

--- 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.cpp
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.cpp
@@ -1,17 +1,28 @@
 class Solution {
 public:
     int totalNumbers(vector<int>& digits) {
-        vector<int> cnt(10, 0);
+        int cnt[10] = {};
         for (int d : digits) cnt[d]++;
 
-        int ans = 0;
-        for (int i = 100; i < 1000; i += 2) {
-            int d1 = i / 100, d2 = (i / 10) % 10, d3 = i % 10;
-            cnt[d1]--; cnt[d2]--; cnt[d3]--;
-            if (cnt[d1] >= 0 && cnt[d2] >= 0 && cnt[d3] >= 0) ans++;
-            cnt[d1]++; cnt[d2]++; cnt[d3]++;
+        int res = 0;
+        for (int a = 1; a <= 9; a++) {
+            if (!cnt[a]) continue;
+            cnt[a]--;
+
+            for (int b = 0; b <= 9; b++) {
+                if (!cnt[b]) continue;
+                cnt[b]--;
+
+                for (int c = 0; c <= 8; c += 2) {
+                    if (cnt[c] > 0) res++;
+                }
+
+                cnt[b]++;
+            }
+
+            cnt[a]++;
         }
 
-        return ans;
+        return res;
     }
 };
